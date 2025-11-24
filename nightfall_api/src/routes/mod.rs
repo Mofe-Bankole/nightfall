09e90a55dd5b2ce3c93099de0fc8ec3050886f97
Mofe-Bankole <bankoleadedotun16@gmail.com>
2025-11-24 12:26:56 +0100

    Update : Added Keys Generation

--- nightfall_api/src/routes/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod auth;
-pub mod transactions;
-pub mod wallet;
--- nightfall_api/src/routes/transactions.rs
+++
@@ -1,8 +0,0 @@
-use std::str::FromStr;
-
-pub async fn transfer(amount : u64 , recipient: &str){
-    let transparent_address_recipient = zcash_address::ZcashAddress::try_from_encoded(recipient)
-        .unwrap()
-        .convert_to_transparent()
-        .unwrap();
-}
